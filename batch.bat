6dab571d9d1e6eabc6b10b6fe4fc19ea2c1f2f87
Breee <74506708+breeev@users.noreply.github.com>
2022-06-19 19:26:59 +0200

    Add batch (don't support spaces in folder names)

---
+++ batch.bat
@@ -0,0 +1,21 @@
+:: fchooser.bat
+:: launches a folder chooser and outputs choice to the console
+:: https://stackoverflow.com/a/15885133/1683264
+
+@echo off
+setlocal
+
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Dossier contenant tes musiques',0,0).self.path""
+
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "src=%%I"
+
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Dossier de destination',0,0).self.path""
+
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "dst=%%I"
+
+setlocal enabledelayedexpansion
+:: echo Copying from !src! to !dst!
+robocopy !src! !dst! *.* /xx /njh
+endlocal
